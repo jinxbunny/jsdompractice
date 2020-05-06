53ee33d6f1a236865e4ddb5dfc475b87263fd886
jinxbunny <info@jmkit.com>
2020-05-06 11:05:24 +0100

    this is my commit comment

---
+++ 4 - querySelector and querySelectorAll/app.js
@@ -0,0 +1,8 @@
+const listItems = document.querySelectorAll("li:nth-child(even)");
+
+console.log(listItems);
+console.log(listItems.length);
+
+for (let i=0; i< listItems.length; i++){
+    listItems[i].style.color="lightgeen";
+}
--- 4 - querySelector and querySelectorAll/challenge.js
+++ 4 - querySelector and querySelectorAll/challenge.js
@@ -1,2 +1,7 @@
 //cycle over the list items and apply colors from the array called colours
 const colours = ['red', 'yellow', 'goldenrod', 'lightgreen', 'blue', 'purple'];
+const list = document.querySelectorAll("li");
+
+for (let i=0; i< colours.length; i++){
+    list[i].style.color = colours[i];
+}
